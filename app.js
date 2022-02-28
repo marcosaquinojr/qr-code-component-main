bd613fb4f30efc2823695708978c0a2c16c0001b
marcosaquinojr <marcosaquinojunior@gmail.com>
2022-02-28 20:05:22 -0300

    cleaning the code

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-function x ()  {
+function generator ()  {
     
     var conteudo = document.getElementById('text').value;
 	var GoogleCharts = 'https://chart.googleapis.com/chart?chs=500x500&cht=qr&chl=';
 	var qrcode = GoogleCharts + conteudo;
-	document.getElementById('qrcode').src = qrcode; 
-  
-    document.getElementById('qrcode').src = qrcode
+	document.getElementById('qrcode').src = qrcode;   
 }
