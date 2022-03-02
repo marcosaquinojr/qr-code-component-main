f86c52bf21c4fa99d423dec350f11ca15183a8c2
marcosaquinojr <marcosaquinojunior@gmail.com>
2022-03-01 23:00:10 -0300

    Att: Download e interações com o usuário 😁

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
+document.getElementById('text').focus();
+document.getElementById("download").disabled = true;
 function generator ()  {
-    
     var conteudo = document.getElementById('text').value;
-	var GoogleCharts = 'https://chart.googleapis.com/chart?chs=500x500&cht=qr&chl=';
-	var qrcode = GoogleCharts + conteudo;
-	document.getElementById('qrcode').src = qrcode;   
+	if(conteudo){
+		var GoogleCharts = 'https://chart.googleapis.com/chart?chs=500x500&cht=qr&chl=';
+		qrcode = GoogleCharts + conteudo;
+		document.getElementById('qrcode').src = qrcode;   
+		swal("Prontinho!", "Seu QR Code foi gerado!", "success");
+		document.getElementById("download").disabled = false;
+	}else{
+		
+		swal("Digite um link para gerar o QR Code!");
+	}
+}
+
+async function download(){
+	const image = await fetch(qrcode)
+	const imageBlog = await image.blob()
+	const imageURL = URL.createObjectURL(imageBlog)
+
+	const a = document.createElement('a');
+  	a.href = imageURL;
+	a.download = 'qrcode'
+	//a.setAttribute('download', 'image.jpg');
+  	document.body.appendChild(a);
+  	a.click();
+  	document.body.removeChild(a);
+	location.reload();
+	
+	  
 }
